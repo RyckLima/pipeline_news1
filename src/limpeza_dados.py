f8ba4b00698792e8d91017b0c881f5f25cd6a4d0
RyckLima <henryquelima000@gmail.com>
2026-03-06 11:55:13 -0300

    refactor: encapsular o módulo de limpeza de dados
    Fiz o encapsulamento, para melhorar reutilização do código.
    o Please enter the commit message for your changes. Lines starting

--- src/limpeza_dados.py
+++ src/limpeza_dados.py
@@ -1,41 +1,48 @@
 import pandas as pd
-from extracao import extrair
+from src.extracao import extrair
 
-#Os dados do source vieram aninhados (um dicionário dentro do outro.), então para transformar em um df usamod json_normalize
-df_news = pd.json_normalize(extrair())
+def limpar (dados, termos):
 
-#remoçao de colunas desnecessárias
-df_news2 = df_news.drop(columns = ['urlToImage' , 'source.id' , 'description'])
+    #Os dados do source vieram aninhados (um dicionário dentro do outro.), então para transformar em um df usamod json_normalize
+    df = pd.json_normalize(dados)
 
-#filtrar linhas para garantir que o conteúdo seja de geopolítica. Utilizando máscara booleana
-termos = (
-    'geopolitics|diplomacy|NATO|OTAN|BRICS|sanctions|embargo|'
-    'border|fronteira|treaty|acordo|summit|cúpula|foreign policy|'
-    'sovereignty|soberania|unilateral|multilateral|hegemony|'
-    'territory|conflict|conflito|warfare|embassy|embaixada|'
-    'intelligence|espionagem|defense|defesa|geostrategy'
-)
+    #remoçao de colunas desnecessárias
+    df_news2 = df.drop(columns = ['urlToImage' , 'source.id' , 'description'])
 
-#case é parametro para maiusculos e minusculos, eu estou aceitando ambos e na para valor vazio.
-filtros_titulo = df_news2['title'].str.contains(termos , case = False , na = False)
-filtros_conteudo = df_news2['content'].str.contains(termos , case = False , na = False)
+    #case é parametro para maiusculos e minusculos, eu estou aceitando ambos e na para valor vazio.
+    filtros_titulo = df_news2['title'].str.contains(termos , case = False , na = False)
+    filtros_conteudo = df_news2['content'].str.contains(termos , case = False , na = False)
 
-#filtragem híbrida:
-df_news3 = df_news2[filtros_titulo|filtros_conteudo]
+    #filtragem híbrida:
+    df_news3 = df_news2[filtros_titulo|filtros_conteudo]
 
-#limpeza de duplicatas.
-df_news4 = df_news3.drop_duplicates().copy()
+    #limpeza de duplicatas.
+    df_news4 = df_news3.drop_duplicates().copy()
 
-#conversão para objeto datatime na coluna de publicação:
-df_news4['publishedAt'] = pd.to_datetime(df_news4['publishedAt'])
+    #conversão para objeto datatime na coluna de publicação:
+    df_news4['publishedAt'] = pd.to_datetime(df_news4['publishedAt'])
 
-#Tratamento de nulos (por tipo):
+    #Tratamento de nulos (por tipo):
 
-regras = {
-    'author': 'Unknown',
-}
+    regras = {
+        'author': 'Unknown',
+    }
 
-df_news4.fillna(value=regras, inplace=True)
+    df_news4.fillna(value=regras, inplace=True)
+
+    return df_news4
+
+if __name__ == '__main__':
+    print('Limpando dados...')
+    #filtrar linhas para garantir que o conteúdo seja de geopolítica. Utilizando máscara booleana
+    termos = (
+        'geopolitics|diplomacy|NATO|OTAN|BRICS|sanctions|embargo|'
+        'border|fronteira|treaty|acordo|summit|cúpula|foreign policy|'
+        'sovereignty|soberania|unilateral|multilateral|hegemony|'
+        'territory|conflict|conflito|warfare|embassy|embaixada|'
+        'intelligence|espionagem|defense|defesa|geostrategy'
+    )
+    print(limpar(extrair() , termos))
 
 
 
