f7e8149453ae5b2f289602b1a7c1ce915214900e
RyckLima <henryquelima000@gmail.com>
2026-03-06 14:00:42 -0300

    style : adicionar a verificação para modularizacao

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from src.extracao import extrair
 from src.limpeza_dados import limpar
 from src.load import subir_dados
 
-termos = (
-        'geopolitics|diplomacy|NATO|OTAN|BRICS|sanctions|embargo|'
-        'border|fronteira|treaty|acordo|summit|cúpula|foreign policy|'
-        'sovereignty|soberania|unilateral|multilateral|hegemony|'
-        'territory|conflict|conflito|warfare|embassy|embaixada|'
-        'intelligence|espionagem|defense|defesa|geostrategy'
-    )
+if __name__ == "__main__":
+    termos = (
+            'geopolitics|diplomacy|NATO|OTAN|BRICS|sanctions|embargo|'
+            'border|fronteira|treaty|acordo|summit|cúpula|foreign policy|'
+            'sovereignty|soberania|unilateral|multilateral|hegemony|'
+            'territory|conflict|conflito|warfare|embassy|embaixada|'
+            'intelligence|espionagem|defense|defesa|geostrategy'
+        )
 
-dados_brutos = extrair ()
-df = limpar(dados_brutos  , termos)
-subir_dados(df)
+    dados_brutos = extrair ()
+    df = limpar(dados_brutos  , termos)
+    subir_dados(df)
