80f5363d0ade54300b5a6c4665360b8c95ee21a6
RyckLima <henryquelima000@gmail.com>
2026-03-06 13:45:41 -0300

    refactor : encapsular nosso módulo de subir dados e Pleasr entee the commit message for your changes. Lines starting

--- src/load.py
+++ src/load.py
@@ -1,27 +1,27 @@
 from sqlalchemy import create_engine , text
-from limpeza_dados import df_news4
 from sqlalchemy.exc import IntegrityError
 
-path_data = 'sqlite:///../data/noticias.db'
-engine = create_engine(path_data)
-query_create_table = """
-CREATE TABLE IF NOT EXISTS noticias_geopolitica (
-    url TEXT PRIMARY KEY,
-    title TEXT,
-    author TEXT,
-    publishedAt TIMESTAMP,
-    content TEXT
-)
-"""
+def subir_dados(df_limpo):
+    path_data = 'sqlite:///./data/noticias.db'
+    engine = create_engine(path_data)
+    query_create_table = """
+    CREATE TABLE IF NOT EXISTS noticias_geopolitica (
+        url TEXT PRIMARY KEY,
+        title TEXT,
+        author TEXT,
+        publishedAt TIMESTAMP,
+        content TEXT
+    )
+    """
 
-# Abrindo a "ponte" e executando o comando
-with engine.connect() as conexao:
-    conexao.execute(text(query_create_table))
+    # Abrindo a "ponte" e executando o comando
+    with engine.connect() as conexao:
+        conexao.execute(text(query_create_table))
 
-#Onde transformo de dataframe para SQL e mando para nosso banco de dados.
-#aqui tem o nome da tabela, o motor que usei, se eu quero escolher entre replace ou append, e para tirar os indices do panda
-try:
-    df_news4.to_sql('noticias_geopolitica', con=engine, if_exists='append', index=False)
-    print("Dados inseridos com sucesso.")
-except IntegrityError:
-    print("Aviso: Algumas URLs já existiam no banco e foram ignoradas.")
+    #Onde transformo de dataframe para SQL e mando para nosso banco de dados.
+    #aqui tem o nome da tabela, o motor que usei, se eu quero escolher entre replace ou append, e para tirar os indices do panda
+    try:
+        df_limpo.to_sql('noticias_geopolitica', con=engine, if_exists='append', index=False)
+        print("Dados inseridos com sucesso.")
+    except IntegrityError:
+        print("Aviso: Algumas URLs já existiam no banco e foram ignoradas.")
